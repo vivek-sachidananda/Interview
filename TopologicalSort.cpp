6b93fb8f2a920293586404c929942af4018d6994
vivek <kimi>
2017-12-17 15:04:01 +0100

    Added the topological sorting

---
+++ TopologicalSort.cpp
@@ -0,0 +1,5 @@
+//
+// Created by vivek on 12/16/2017.
+//
+
+#include "TopologicalSort.h"
--- main.cpp
+++ main.cpp
@@ -4,35 +4,19 @@
 #include <vector>
 #include <unordered_map>
 
+#include "TopologicalSort.h"
+
 using namespace std;
 
 int main()
 {
     cout << "test" << endl;
 
+    TopologicalSort aTopo;
 
-    std::unordered_map<char, size_t> aMap;
+    aTopo.runTopologicalSort();
 
-    std::string aStr1, aStr2;
-    aStr1 = "abbc";
-    aStr2 = "babc";
-
-    std::vector<char> aVec1(aStr1.begin(), aStr1.end()), aVect2(aStr2.begin(), aStr2.end());
-
-    for (size_t i = 0; i < aStr1.length(); ++i)
-    {
-        ++aMap[aVec1[i]];
-        --aMap[aVect2[i]];
-    }
-
-    std::unordered_map<char, size_t>::const_iterator aIter = aMap.begin();
-    std::unordered_map<char, size_t>::const_iterator aEndIter = aMap.end();
-
-    while (aIter != aEndIter)
-    {
-        cout << aIter->first << " " << aIter->second << endl;
-        ++aIter;
-    }
+    
 
     return 0;
 }
