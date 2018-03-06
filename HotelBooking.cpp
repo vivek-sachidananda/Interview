03bf58244a2d0ecfc01be8ce987b26554aaa61ea
viveksachidananda <vivekkimi@yahoo.com>
2018-03-06 20:24:09 +0100

    Update HotelBooking.cpp

--- HotelBooking.cpp
+++ HotelBooking.cpp
@@ -52,3 +52,44 @@ bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int K) {
     
     return true;
 }
+
+
+// Fastest
+
+/*bool comp(pair<int, int> j, pair<int, int> i){
+    cout<<"i="<<i.first<<" "<<i.second<<"j="<<j.first<<" "<<j.second<<endl;
+    if(i.first > j.first){
+        return true;
+    }
+    else if(i.first == j.first){
+        if(i.second > j.second){
+            return true;
+        }
+    }
+    return false;
+}*/
+
+bool Solution::hotel(vector<int> &arrive, vector<int> &depart, int K) {
+     
+    int arr = 0;
+    int dep = 0;
+    
+    sort(arrive.begin(), arrive.end());
+    sort(depart.begin(), depart.end());
+    
+    while(arr < arrive.size()){
+        K--;
+        arr++;
+        if(K == 0){
+            if(arrive[arr] >= depart[dep]){
+                K++;
+                dep++;
+            }    
+        }
+        if(K < 0){
+            return false;
+        }
+    }
+        
+    return true;
+}
